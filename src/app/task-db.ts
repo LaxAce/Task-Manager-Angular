7a0f4d03a7935933dff3666baf23a71f04c98c05
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-01-29 21:35:54 +0100

    first commit

---
+++ src/app/task-db.ts
@@ -0,0 +1,34 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'School Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Job Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: 'Football Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: false,
+    },
+    {
+        id: 5,
+        text: 'Prayer Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: false,
+    }
+]
